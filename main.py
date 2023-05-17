e9101ac2f5924bdb98053ffca6b6f9dbd97cde93
ali ibrahimli <alibrahimli041@gmail.com>
2023-05-17 17:23:34 +0400

    sending email done

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import time
 import requests
 import selectorlib
+import smtplib
+import ssl
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
@@ -18,8 +21,19 @@ def extract(source):
     return value
 
 
-def send_email():
-    print("Email was sent!")
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "alibrahimli041@gmail.com"
+    password = "pckxbssmffhdbebb"
+
+    receiver = "alibrahimli041@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 def store(extracted):
     with open("data.txt", 'a') as file:
         file.write(extracted + "\n")
@@ -29,12 +43,14 @@ def read(extracted):
     with open("data.txt", 'r') as file:
         return file.read()
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
 
-    content = read(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email()
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="Hey, new even was found!")
+        time.sleep(0.2)

97db6fd7db6ff5d0bd3a42badf6d8e4f72276746
ali ibrahimli <alibrahimli041@gmail.com>
2023-05-18 16:52:04 +0400

    I connected my program to sql database and coming data goes to DB and also when new data comes it sends email to me.

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import requests
 import selectorlib
 import smtplib
 import ssl
+import  sqlite3
 
 
 "INSERT INTO events VALUES ('Tigers', 'Tiger City', '2088.10.14')"
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
-
+connection = sqlite3.connect("data.db")
 def scrape(url):
     """Scrape the page source from the URL"""
 
@@ -39,14 +40,22 @@ def send_email(message):
 
 
 def store(extracted):
-    with open("data.txt", 'a') as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 
-def read():
-    with open("data.txt", 'r') as file:
-        return file.read()
-
+def read(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
 
 if __name__ == "__main__":
     while True:
@@ -54,9 +63,9 @@ if __name__ == "__main__":
         extracted = extract(scraped)
         print(extracted)
 
-        content = read()
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="Hey, new even was found!")
         time.sleep(0.2)
